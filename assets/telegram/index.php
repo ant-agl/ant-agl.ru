e62228baa3de4fb2438411088b5bc3faffcda321
Денис Сухоярский <densuh68@gmail.com>
2023-09-24 14:29:21 +0300

    telegram

--- assets/telegram/index.php
+++ assets/telegram/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/TelegramConnector.php';
 
-$token = "";
+$token = "6568622203:AAGSlCG1tv3LaVWbzI5ZkvzLs78PQ7zL8xE";
 
 $name = $_POST['name'];
 $email = $_POST['email'];
@@ -17,7 +17,7 @@ try {
     
     $telegramManager ->__construct($token);
 
-    $telegramManager->sendMessage(983206088, $text);
+    $telegramManager->sendMessage(710899516, $text);
 
 } catch (Exception $e) {
     file_put_contents("MAIN_ERROR_LOG.txt", 'Ошибка: ' . $e->getMessage() . PHP_EOL . 'Код ошибки: ' . $e->getCode() . " " . PHP_EOL . $e->getFile() . " : " . $e->getLine());
